538afb0038182ca218260053881de67b1725f04d
hayyrs66 <rayrtsx@proton.me>
2024-04-06 15:31:21 -0600

    BRL to USD

--- src/modules/BrazilianRealToDollar.java
+++ src/modules/BrazilianRealToDollar.java
@@ -1,10 +1,15 @@
 package modules;
 
+import api.ExchangeAPI;
 import interfaces.Command;
+
+import java.io.IOException;
 
 public class BrazilianRealToDollar implements Command {
     @Override
-    public void execute(double amount) {
-
+    public void execute(double amount) throws IOException, InterruptedException {
+        double conversion = ExchangeAPI.getConversion("BRL");
+        double convertedValue = amount / conversion;
+        System.out.println(amount + " BRL "  + " are " + convertedValue + " USD ");
     }
 }
