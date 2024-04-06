f849627a8de34e83e960e9ae800bb457e760f4c5
hayyrs66 <rayrtsx@proton.me>
2024-04-06 15:32:51 -0600

    COP to USD

--- src/modules/ColombianPesoToDollar.java
+++ src/modules/ColombianPesoToDollar.java
@@ -1,10 +1,15 @@
 package modules;
 
+import api.ExchangeAPI;
 import interfaces.Command;
+
+import java.io.IOException;
 
 public class ColombianPesoToDollar implements Command {
     @Override
-    public void execute(double amount) {
-
+    public void execute(double amount) throws IOException, InterruptedException {
+        double conversion = ExchangeAPI.getConversion("COP");
+        double convertedValue = amount / conversion;
+        System.out.println(amount + " COP "  + " are " + convertedValue + " USD ");
     }
 }
