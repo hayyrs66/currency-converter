5ac2a336252a03f335c7339821e0fbf5f78d98d0
hayyrs66 <rayrtsx@proton.me>
2024-04-06 15:29:29 -0600

    ARS to USD

--- src/modules/ArgentinePesoToDollar.java
+++ src/modules/ArgentinePesoToDollar.java
@@ -1,11 +1,16 @@
 package modules;
 
+import api.ExchangeAPI;
 import interfaces.Command;
+
+import java.io.IOException;
 
 public class ArgentinePesoToDollar implements Command {
 
     @Override
-    public void execute(double amount) {
-
+    public void execute(double amount) throws IOException, InterruptedException {
+        double conversion = ExchangeAPI.getConversion("ARS");
+        double convertedValue = amount / conversion;
+        System.out.println(amount + " ARS "  + " are " + convertedValue + " Dollars ");
     }
 }
